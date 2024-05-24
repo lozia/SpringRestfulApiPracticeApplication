8655e6fe15d095a7a939bb259d67b56043dbc274
lozia <lizongyao9745@hotmail.com>
2024-05-23 23:38:56 -0700

    Prepare for aws deploy

--- src/main/java/com/example/springbootrestfulapipractice/SpringBootRestfulApiPracticeApplication.java
+++ src/main/java/com/example/springbootrestfulapipractice/SpringBootRestfulApiPracticeApplication.java
@@ -2,6 +2,8 @@ package com.example.springbootrestfulapipractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootRestfulApiPracticeApplication {
@@ -10,4 +12,12 @@ public class SpringBootRestfulApiPracticeApplication {
         SpringApplication.run(SpringBootRestfulApiPracticeApplication.class, args);
     }
 
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/api/student").allowedOrigins("http://localhost:4200");
+//            }
+//        };
+//    }
 }
--- src/main/java/com/example/springbootrestfulapipractice/controller/StudentController.java
+++ src/main/java/com/example/springbootrestfulapipractice/controller/StudentController.java
@@ -4,51 +4,63 @@ import com.example.springbootrestfulapipractice.dto.StudentDto;
 import com.example.springbootrestfulapipractice.service.impl.StudentService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/student")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping
     public ResponseEntity<StudentDto> createStudent(@RequestBody StudentDto studentDto){
+        if (studentDto.getEmail().isEmpty() || studentDto.getFirstName().isEmpty() || studentDto.getLastName().isEmpty()){
+            return new ResponseEntity<>(studentDto, HttpStatus.NOT_ACCEPTABLE);
+        }
         StudentDto createdStudent = studentService.createStudent(studentDto);
         return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
     }
 
+//    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("{id}")
     public ResponseEntity<StudentDto> getStudent(@PathVariable long id){
         StudentDto studentDto = studentService.getStudent(id);
         return ResponseEntity.ok(studentDto);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<StudentDto>> getAllStudents(){
         List<StudentDto> students = studentService.getAllStudents();
         return ResponseEntity.ok(students);
     }
 
+
+//    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("{id}")
     public ResponseEntity<StudentDto> updateStudent(@PathVariable("id") long id, @RequestBody StudentDto studentDto ){
         StudentDto updated = studentService.updateStudent(id, studentDto);
         return new ResponseEntity<>(updated, HttpStatus.ACCEPTED);
     }
 
-
+//    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping(value = "{id}")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") long id){
         StudentDto studentDto = studentService.deleteStudent(id);
-        return new ResponseEntity<>("Student with id: "+id+" is deleted.", HttpStatus.OK);
+        String temp = "a";
+
+        return new ResponseEntity<>("{\"message\": \"Student with id: "+id+" is deleted.\"}", HttpStatus.OK);
     }
 }
